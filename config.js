96485684942878b4bec483331e6a1bb89fdc6841
Nastya07s <nastya.kotovich07@gmail.com>
2022-09-12 01:36:08 +0200

    init: work with ebay api

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  SANDBOX: {
+    clientId: process.env.CLIENT_ID_SANDBOX,
+    clientSecret: process.env.CLIENT_SECRET_SANDBOX,
+    devid: process.env.DEV_ID_SANDBOX,
+    redirectUri: process.env.REDIRECT_URI_SANDBOX,
+    baseUrl: 'api.sandbox.ebay.com',
+  },
+  PRODUCTION: {
+    clientId: process.env.CLIENT_ID_Production,
+    clientSecret: process.env.CLIENT_SECRET_PRODUCTION,
+    devid: process.env.DEV_ID_PRODUCTION,
+    redirectUri: process.env.REDIRECT_URI_PRODUCTION,
+    baseUrl: 'api.ebay.com',
+  },
+};
---
+++ index.ts
@@ -0,0 +1,72 @@
+import express, { Express, Request, Response } from 'express';
+
+const EbayAuthToken = require('ebay-oauth-nodejs-client');
+const crypto = require('crypto');
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = require('./../config.js');
+
+const app: Express = express();
+const port = 3000;
+
+const ebayAuthToken = new EbayAuthToken({
+  clientId: config.PRODUCTION.clientId,
+  clientSecret: config.PRODUCTION.clientSecret,
+  redirectUri: config.PRODUCTION.redirectUri,
+  // env: 'SANDBOX',
+});
+
+app.get('/dresses', async (req: Request, res: Response) => {
+  const tokenP = await ebayAuthToken.getApplicationToken('PRODUCTION');
+
+  const response = await axios.get(
+    'https://api.ebay.com/buy/browse/v1/item_summary/search?category_ids=63861&limit=200',
+    {
+      headers: {
+        Authorization: 'Bearer ' + tokenP,
+      },
+    },
+  );
+
+  console.log('itemSummaries: ', response.data.itemSummaries.length);
+
+  res.send(response.data);
+});
+
+app.post('/', (req: Request, res: Response) => {
+  res.status(200).send();
+});
+
+app.get('/', async (req: Request, res: Response) => {
+  const challengeCode = req.query.challenge_code;
+
+  if (challengeCode) {
+    try {
+      const verificationToken = process.env.VERIFICATION_TOKEN;
+      const endpoint = process.env.ENDPOINT;
+
+      const hash = crypto.createHash('sha256');
+
+      hash.update(challengeCode);
+      hash.update(verificationToken);
+      hash.update(endpoint);
+
+      const responseHash = hash.digest('hex');
+
+      const result = Buffer.from(responseHash).toString();
+
+      res.status(200).send({
+        challengeResponse: result,
+      });
+    } catch (e) {
+      console.error(`Endpoint validation failure: ${e}`);
+      res.status(500).send();
+    }
+  }
+});
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${process.env.PORT || port}`);
+});
